204c3707e53193b6124b2f81aaa437a7c74657b4
piotrbartnik <piotr.bartnik@hotmail.com>
2019-05-16 23:02:03 +0200

    working with react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Layout from '../src/components/Layout/Layout';
+import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <div>
-     
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.modules.css';
+import classes from './index.modules.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
