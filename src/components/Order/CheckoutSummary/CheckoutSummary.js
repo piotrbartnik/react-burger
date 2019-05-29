03e48f94bee538655f11dbc8b9db3e887a70dce2
piotrbartnik <piotr.bartnik@hotmail.com>
2019-05-29 22:22:42 +0200

    added order

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.modules.css';
+
 
 const checkoutSummary = (props) => {
   return (
-    <div>
+    <div className={classes.CheckoutSummary}>
       <h1> We hope it tastes well!</h1>
       <div style={{ width: '300px', height: '300px', margin: 'auto' }}>
-        <Burger />
+        <Burger ingredients={props.ingredients}/>
       </div>
+      <Button btnType="Danger" clicked>Cancel</Button>
+      <Button btnType="Success" clicked>Continue</Button>
     </div>
   )
 };
