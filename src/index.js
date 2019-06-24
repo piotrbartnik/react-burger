493951794656cfc54653f743b1c7f6e2a0a25c50
piotrbartnik <piotr.bartnik@hotmail.com>
2019-06-25 00:04:52 +0200

    working with thunk

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import {createStore } from 'redux';
+import {createStore, applyMiddleware, compose } from 'redux';
 import reducer from './store/reducers/burgerBuilder';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 
 const app = (
