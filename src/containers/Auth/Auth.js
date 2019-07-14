1092f6fd8bc724ca5af9bfa2a346fc0ffc677acb
piotrbartnik <piotr.bartnik@hotmail.com>
2019-07-14 10:16:23 +0200

    fixed auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -64,14 +64,14 @@ class Auth extends Component {
   inputChangeHandler = (event, controlName) => {
     const updatedControls = {
       ...this.state.controls,
-      controlName: {
-        ...this.state.constrols[controlName],
+      [controlName]: {
+        ...this.state.controls[controlName],
         value: event.target.value,
         valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
         touched: true
       }
     }
-    this.setState({constrols: updatedControls})
+    this.setState({controls: updatedControls})
   };
 
   submitHandler = (event) => {
