e26265ce03337906aaabf6952a31a2b83ae92147
piotrbartnik <piotr.bartnik@hotmail.com>
2019-07-15 21:23:19 +0200

    adding authorization

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionsTypes';
+import axios from 'axios';
 
 export const authStart = () => {
   return {
@@ -23,5 +24,11 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnsecureToken: true
+    }
+    axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyDNcE6kIAq2j4ISbwYdzCj5XIp13neLWRg')
   }
 }
