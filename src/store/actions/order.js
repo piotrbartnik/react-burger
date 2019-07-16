3b308e8bd70baa87dbc0ff168ffefac8f620e999
piotrbartnik <piotr.bartnik@hotmail.com>
2019-07-16 22:24:41 +0200

    added auth to order

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -22,10 +22,10 @@ export const purchaseBurgerStart = () => {
   }
 }
 
-export const purchaseBurger = (orderData) => {
+export const purchaseBurger = (orderData, token) => {
   return dispatch => {
     dispatch(purchaseBurgerStart());
-    axios.post('/order.json', orderData)
+    axios.post('/order.json?auth=' + token, orderData)
       .then(response => {
         console.log(response.data)
         dispatch(purchaseBurgerSuccess(response.data.name, orderData))
@@ -62,10 +62,10 @@ export const fetchOrderStart = () => {
   }
 }
 
-export const fetchOrders = () => {
+export const fetchOrders = (token) => {
   return dispatch => {
     dispatch(fetchOrderStart())
-    axios.get('/order.json')
+    axios.get('/order.json?auth=' + token)
     .then(response => {
       const fetchedOrders = [];
       for (let key in response.data) {
