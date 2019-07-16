86173aa0575f25f5ee5ee1cf923fdd007f3e09cd
piotrbartnik <piotr.bartnik@hotmail.com>
2019-07-16 23:05:30 +0200

    added logout button working

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch } from 'react-router-dom';
 import Orders from './containers/Checkout/Orders/Orders';
-import Auth from './containers/Auth/Auth'
+import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 
 class App extends Component {
   state = {
@@ -19,6 +20,7 @@ class App extends Component {
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
