c55bb66aa83bfd96c4b2c292329e6e5e6bc481e6
piotrbartnik <piotr.bartnik@hotmail.com>
2019-07-18 22:12:21 +0200

    testing containers

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import * as actions from '../../store/actions/index';
 import axios from '../../axios-order';
 
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   // constructor(props) {
   //     super(props);
   //     this.state = {...}
---
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,20 @@
+import { BurgerBuilder } from "./BurgerBuilder";
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+import React from 'react';
+
+configure({adapter: new Adapter});
+
+describe('<BurgerBuilder />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<BurgerBuilder onInitIngredients={()=>{}}/>)
+  });
+
+  it('should render <BC> when receiving ingredients', () => {
+    wrapper.setProps({ings: {salad: 0}});
+    expect(wrapper.find(BuildControls)).toHaveLength(1)
+  })
+})
