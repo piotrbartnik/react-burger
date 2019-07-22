9b1fd82983a226f522b421143828634d6f65f0be
piotrbartnik <piotr.bartnik@hotmail.com>
2019-07-22 20:53:26 +0200

    changed build in gitignore

---
+++ build/precache-manifest.e68ece63495d099c2e1ccf8bde3d7388.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be7d230fa7e089722ffb1472dfefc63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aacc5298dc65d49658c",
+    "url": "/static/css/3.c3bed146.chunk.css"
+  },
+  {
+    "revision": "03895129580a8680d26d",
+    "url": "/static/css/4.8f6671ab.chunk.css"
+  },
+  {
+    "revision": "a3baecb7fb4dc76a895f",
+    "url": "/static/css/5.448f5ea2.chunk.css"
+  },
+  {
+    "revision": "d0ddd3778d177e7c75c8",
+    "url": "/static/css/main.9df6de5b.chunk.css"
+  },
+  {
+    "revision": "e3375de5836c7ee10b20",
+    "url": "/static/js/2.fbb31ec7.chunk.js"
+  },
+  {
+    "revision": "3aacc5298dc65d49658c",
+    "url": "/static/js/3.7f035b12.chunk.js"
+  },
+  {
+    "revision": "03895129580a8680d26d",
+    "url": "/static/js/4.43effb54.chunk.js"
+  },
+  {
+    "revision": "a3baecb7fb4dc76a895f",
+    "url": "/static/js/5.cd4f3c96.chunk.js"
+  },
+  {
+    "revision": "d0ddd3778d177e7c75c8",
+    "url": "/static/js/main.8c6f6c99.chunk.js"
+  },
+  {
+    "revision": "b6e4a7e32fe42bf88522",
+    "url": "/static/js/runtime~main.37624246.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/logo.b8503d26.png"
+  }
+]);
