d0af9d73b802e4c634fd3666ffa869e685df09ae
piotrbartnik <piotr.bartnik@hotmail.com>
2019-07-22 20:56:59 +0200

    created docs

---
+++ docs/precache-manifest.b48dd0c27694b7e5b09bbfa5103f8d91.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec367d96186962a05e0238bf5b331377",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aacc5298dc65d49658c",
+    "url": "/static/css/3.c3bed146.chunk.css"
+  },
+  {
+    "revision": "03895129580a8680d26d",
+    "url": "/static/css/4.8f6671ab.chunk.css"
+  },
+  {
+    "revision": "a3baecb7fb4dc76a895f",
+    "url": "/static/css/5.448f5ea2.chunk.css"
+  },
+  {
+    "revision": "1599d871783151f137ba",
+    "url": "/static/css/main.9df6de5b.chunk.css"
+  },
+  {
+    "revision": "e3375de5836c7ee10b20",
+    "url": "/static/js/2.fbb31ec7.chunk.js"
+  },
+  {
+    "revision": "3aacc5298dc65d49658c",
+    "url": "/static/js/3.7f035b12.chunk.js"
+  },
+  {
+    "revision": "03895129580a8680d26d",
+    "url": "/static/js/4.43effb54.chunk.js"
+  },
+  {
+    "revision": "a3baecb7fb4dc76a895f",
+    "url": "/static/js/5.cd4f3c96.chunk.js"
+  },
+  {
+    "revision": "1599d871783151f137ba",
+    "url": "/static/js/main.a6e094a8.chunk.js"
+  },
+  {
+    "revision": "b6e4a7e32fe42bf88522",
+    "url": "/static/js/runtime~main.37624246.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/logo.b8503d26.png"
+  }
+]);
